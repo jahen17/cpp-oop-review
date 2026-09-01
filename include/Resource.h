5b81364c3b913ce318e59bd4697743b860bb416d
jahen17 <jahen_1705@hotmail.com>
2026-09-01 15:13:49 -0500

    docs(cmake-list): update rEADME.md, add project-level documentation files, including class diagram, adrs, and user stories

--- include/Resource.h
+++ include/Resource.h
@@ -5,11 +5,15 @@
 #include <string_view>
 
 /**
+ * @brief Abstract base class representing a resource used in executable tasks or processes.
+ *
+ * This class defines the interface for resources that can be allocated, released, and used.
+ * The resources can be consumable (e.g., memory) or usable (e.g., CPU).
  *
  */
 class Resource {
 public:
-    enum class Type { Consumable, Usable};
+    enum class Type { Consumable, Usable };
 
 protected:
     std::string name;
@@ -17,16 +21,14 @@ protected:
     Type resourceType;
 
 public:
-    Resource (std::string_view name, Type type);
+    Resource(std::string_view name, Type type);
     virtual ~Resource() = default;
-    [[ndiscard]] std::string_view getName() const;
-    [[nodiscard]] bool isAvailableForUse() const = 0;
-    virtual void allocate()=0;
-    virtual void release()=0;
-    virtual void use () const = 0;
+    [[nodiscard]] std::string_view getName() const;
+    [[nodiscard]] virtual bool isAvailableForUse() const = 0;
+    virtual void allocate() = 0;
+    virtual void release() = 0;
+    virtual void use() const = 0;
     [[nodiscard]] virtual std::string describeUsage() const = 0;
-    [[nodiscard]] Type gestResourceType() const;
-
+    [[nodiscard]] Type getResourceType() const;
 };
-
 #endif //RESOURCE_H
